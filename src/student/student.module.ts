9941342454fed743e9c3fea33e627b3573c1a4bb
Prasenjit Sutradhar <prasenjitsutradhar3433@gmail.com>
2023-08-14 21:42:39 +0530

    createStudent mutation

--- src/student/student.module.ts
+++ src/student/student.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Student } from './student.entity';
 import { StudentService } from './student.service';
 import { StudentRepository } from './student.repository';
+import { StudentResolver } from './student.resolver';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Student])],
-  providers: [StudentService, StudentRepository],
+  providers: [StudentResolver, StudentService, StudentRepository],
 })
 export class StudentModule {}
--- src/student/student.service.ts
+++ src/student/student.service.ts
@@ -1,4 +1,24 @@
 import { Injectable } from '@nestjs/common';
+import { v4 as uuid } from 'uuid';
+import { StudentRepository } from './student.repository';
+import { CreateStudentInput } from './create-student.input';
+import { Student } from './student.entity';
 
 @Injectable()
-export class StudentService {}
+export class StudentService {
+  constructor(private studentRepository: StudentRepository) {}
+
+  async createStudent(
+    createStudentInput: CreateStudentInput,
+  ): Promise<Student> {
+    const { firstName, lastName } = createStudentInput;
+
+    const student = this.studentRepository.create({
+      id: uuid(),
+      firstName,
+      lastName,
+    });
+
+    return this.studentRepository.save(student);
+  }
+}
