94cc489458b1e986e05e8eb957cfab85f9ede346
Prasenjit Sutradhar <prasenjitsutradhar3433@gmail.com>
2023-08-14 13:20:51 +0530

    input validation of createLesson mutation

--- src/lesson/lesson.service.ts
+++ src/lesson/lesson.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { LessonRepository } from './lesson.repository';
 import { v4 as uuid } from 'uuid';
 import { Lesson } from './lesson.entity';
+import { CreateLessonInput } from './lesson.input';
 
 @Injectable()
 export class LessonService {
@@ -11,7 +12,9 @@ export class LessonService {
     return this.lessonRepository.findOne({ where: { id } });
   }
 
-  async createLesson(name, startDate, endDate): Promise<Lesson> {
+  async createLesson(createLessonInput: CreateLessonInput): Promise<Lesson> {
+    const { name, startDate, endDate } = createLessonInput;
+
     const lesson = this.lessonRepository.create({
       id: uuid(),
       name,
